121ef6b0521f8f90be996bd4d83e2dc2cbcd9abc
Nana-Sensei <nanaquojoe@gmail.com>
2023-09-21 13:48:41 +0000

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+
+  {
+      name: "No Privacy",
+      artist: "Chris Brown",
+      img: "CB",
+      src: "CB",
+  },
+  
+  {
+      name: "Bad Habits",
+      artist: "Ed Sheeran",
+      img: "ES",
+      src: "ES"
+  },
+
+  {
+      name: "As it was",
+      artist: "Harry Styles",
+      img: "HS",
+      src: "HS"
+  },
+
+  {
+      name: "Peaches",
+      artist: "Justin Bieber",
+      img: "JB",
+      src: "JB"
+  },
+
+  {
+      name: "Stay",
+      artist: "Kid Laroi ft Justin Bieber",
+      img: "KL",
+      src: "KL"
+  },
+ 
+  
+  ];
